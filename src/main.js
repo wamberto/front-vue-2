4851aaca9128c5ab9f29994fb7ee015c0b864dba
wamberto <wambertof@gmail.com>
2023-02-03 14:51:36 -0300

    Criando novo projjeto Vue na versao 2

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
 import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
-
+import VueSweetalert2 from 'vue-sweetalert2';
+import 'bootstrap/dist/css/bootrat.min.css';
 Vue.config.productionTip = false;
 
 new Vue({
