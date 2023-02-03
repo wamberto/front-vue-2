91d3ec9d4c7e76d5bb1e4fb5786deec0765bc6b2
wamberto <wambertof@gmail.com>
2023-02-03 15:44:38 -0300

    Importacal do bootstrap e sweetalert2

--- src/main.js
+++ src/main.js
@@ -1,9 +1,14 @@
 import Vue from 'vue';
+
+import VueSweetalert2 from 'vue-sweetalert2';
 import App from './App.vue';
 import router from './router';
-import VueSweetalert2 from 'vue-sweetalert2';
-import 'bootstrap/dist/css/bootrat.min.css';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 Vue.config.productionTip = false;
+
+Vue.use(VueSweetalert2);
 
 new Vue({
   router,
